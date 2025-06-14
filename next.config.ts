b5e6a2428bd4618424e11feea8de3b12de27714b
dingdududu <lyn668@gmail.com>
2025-06-14 23:46:00 +0800

    列行提交

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
-import type { NextConfig } from "next";
+// import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+// const nextConfig: NextConfig = {
+//   /* config options here */
+//   devIndicators: false,
+// };
+
+// export default nextConfig;
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  devIndicators: false,
 };
 
-export default nextConfig;
+module.exports = nextConfig;
